de4db22cfbe9da87a913ca76ea981faab305a085
Hiroyuki Uchida <hiroyuki.uchida@dena.com>
2017-03-17 10:39:56 +0900

    add baz counter

--- test2.cs
+++ test2.cs
@@ -4,9 +4,12 @@ namespace Foo
 {
     public class Bar
     {
+        private int mCounter;
+
         public void Baz()
         {
             Debug.Log("test");
+            mCounter++;
         }
     }
 }
